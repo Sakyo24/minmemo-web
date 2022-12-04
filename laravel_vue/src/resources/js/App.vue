64bdbf52cf5b55543e71f1291ff1d9aa3e8a50b5
Migisan <ukyo.business.desktop@gmail.com>
2022-12-05 00:00:10 +0900

    feat:Vue3インストール

---
+++ laravel_vue/src/resources/js/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{ message }}</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const message = ref('こんにちは');
+
+    return {
+      message,
+    };
+  },
+};
+</script>
