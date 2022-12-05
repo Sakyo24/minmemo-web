deac07dcf80f08544c18f3faff7caf143ffced78
Migisan <ukyo.business.desktop@gmail.com>
2022-12-05 23:29:51 +0900

    feat:Vuex4インストール

--- laravel_vue/src/resources/js/App.vue
+++ laravel_vue/src/resources/js/App.vue
@@ -1,16 +1,22 @@
 <template>
-  <div>{{ message }}</div>
+  <div>
+    <div>{{ message }}</div>
+    <div>{{ test }}</div>
+  </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
+import { useStore } from 'vuex';
 
 export default {
   setup() {
     const message = ref('こんにちは');
+    const store = useStore();
 
     return {
       message,
+      test: computed(() => store.state.test.message),
     };
   },
 };
--- laravel_vue/src/resources/js/app.js
+++ laravel_vue/src/resources/js/app.js
@@ -1,8 +1,8 @@
 import './bootstrap';
-
 import { createApp } from 'vue';
 import App from './App.vue';
+import store from './store';
 
 const app = createApp(App);
-
+app.use(store);
 app.mount('#app');
