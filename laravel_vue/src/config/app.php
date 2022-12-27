3ee9e2f3c094a29c97e2b14aeb7d1caf75ff2afc
Migisan <ukyo.business.desktop@gmail.com>
2022-12-27 23:09:31 +0900

    fix:fakerの日本語化

--- laravel_vue/src/config/app.php
+++ laravel_vue/src/config/app.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Support\Facades\Facade;
 
 return [
@@ -41,7 +43,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool)env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------
@@ -108,7 +110,7 @@ return [
     |
     */
 
-    'faker_locale' => 'en_US',
+    'faker_locale' => 'ja_JP',
 
     /*
     |--------------------------------------------------------------------------
