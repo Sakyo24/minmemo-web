0ddb4d570d065173809882fcca89a05b769aa062
Migisan <ukyo.business.desktop@gmail.com>
2023-01-01 22:18:43 +0900

    add:AuthApplicationService作成

---
+++ laravel_vue/src/app/ApplicationServices/AuthApplicationServiceInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\ApplicationServices;
+
+use App\Models\User;
+use Illuminate\Auth\AuthenticationException;
+use RuntimeException;
+
+interface AuthApplicationServiceInterface
+{
+    /**
+     * ユーザー登録処理
+     *
+     * @param array $input
+     * @return User
+     * @throws RuntimeException
+     */
+    public function register(array $input): User;
+
+    /**
+     * ログイン処理
+     *
+     * @param array $input
+     * @return User
+     * @throws AuthenticationException
+     */
+    public function login(array $input): User;
+}
---
+++ laravel_vue/src/tests/Unit/ApplicationServices/AuthApplicationServiceTest.php
@@ -0,0 +1,193 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\ApplicationServices;
+
+use App\ApplicationServices\AuthApplicationServiceInterface;
+use App\Models\User;
+use App\Repositories\UserRepositoryInterface;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
+use Mockery;
+use RuntimeException;
+use Tests\TestCase;
+
+class AuthApplicationServiceTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @var UserRepositoryInterface $user_repository_mock
+     */
+    private UserRepositoryInterface $user_repository_mock;
+
+    /**
+     * @var User $user
+     */
+    private User $user;
+
+    /**
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user_repository_mock = Mockery::mock(UserRepositoryInterface::class);
+
+        $this->user = User::factory()->create();
+    }
+
+    /**
+     * @return void
+     */
+    public function testRegister(): void
+    {
+        // データ
+        $expected_name = 'ユーザー名';
+        $expected_email = 'test@exmaple.com';
+        $expected_password = 'password';
+        $expected_user = User::factory()->make([
+            'name' => $expected_name,
+            'email' => $expected_email,
+            'password' => Hash::make($expected_password),
+        ]);
+
+        // モックの設定
+        $this->user_repository_mock->shouldReceive('create')
+            ->once()
+            ->with(Mockery::on(function ($actual) use ($expected_name, $expected_email, $expected_password) {
+                $this->assertSame($expected_name, $actual['name']);
+                $this->assertSame($expected_email, $actual['email']);
+                $this->assertTrue(Hash::check($expected_password, $actual['password']));
+
+                return true;
+            }))
+            ->andReturn($expected_user);
+
+        $this->app->instance(UserRepositoryInterface::class, $this->user_repository_mock);
+
+        // インスタンス生成
+        $auth_application_service = $this->app->make(AuthApplicationServiceInterface::class);
+
+        // 実行
+        $user = $auth_application_service->register(
+            [
+                'name' => $expected_name,
+                'email' => $expected_email,
+                'password' => $expected_password,
+            ]
+        );
+
+        // 検証
+        $this->assertInstanceOf(User::class, $user);
+        $this->assertSame($expected_name, $user->name);
+        $this->assertSame($expected_email, $user->email);
+    }
+
+    /**
+     * @return void
+     */
+    public function testRegisterRuntimeException(): void
+    {
+        // データ
+        $expected_name = 'ユーザー名';
+        $expected_email = 'test@exmaple.com';
+        $expected_password = 'password';
+
+        // モックの設定
+        $this->user_repository_mock->shouldReceive('create')
+            ->once()
+            ->with(Mockery::on(function ($actual) use ($expected_name, $expected_email, $expected_password) {
+                $this->assertSame($expected_name, $actual['name']);
+                $this->assertSame($expected_email, $actual['email']);
+                $this->assertTrue(Hash::check($expected_password, $actual['password']));
+
+                return true;
+            }))
+            ->andThrow(RuntimeException::class);
+
+        $this->app->instance(UserRepositoryInterface::class, $this->user_repository_mock);
+
+        // インスタンス生成
+        $auth_application_service = $this->app->make(AuthApplicationServiceInterface::class);
+
+        // 検証
+        $this->expectException(RuntimeException::class);
+
+        // 実行
+        $auth_application_service->register(
+            [
+                'name' => $expected_name,
+                'email' => $expected_email,
+                'password' => $expected_password,
+            ]
+        );
+    }
+
+    /**
+     * TODO: DBに依存しているテスト
+     *
+     * @return void
+     */
+    public function testLogin(): void
+    {
+        // データ
+        $expected_name = $this->user->name;
+        $expected_email = $this->user->email;
+        $expected_password = 'password';
+        $expected_user = $this->user;
+
+        // モックの設定
+        $this->user_repository_mock->shouldReceive('findById')
+            ->once()
+            ->with($expected_user->id)
+            ->andReturn($expected_user);
+
+        $this->app->instance(UserRepositoryInterface::class, $this->user_repository_mock);
+
+        // インスタンス生成
+        $auth_application_service = $this->app->make(AuthApplicationServiceInterface::class);
+
+        // 実行
+        $user = $auth_application_service->login(
+            [
+                'email' => $expected_email,
+                'password' => $expected_password,
+            ]
+        );
+
+        // 検証
+        $this->assertInstanceOf(User::class, $user);
+        $this->assertSame($expected_name, $user->name);
+        $this->assertSame($expected_email, $user->email);
+    }
+
+    /**
+     * TODO: DBに依存しているテスト
+     *
+     * @return void
+     */
+    public function testLoginAuthenticationException(): void
+    {
+        // データ
+        $expected_email = $this->user->email;
+        $expected_password = 'password1';
+
+        // インスタンス生成
+        $auth_application_service = $this->app->make(AuthApplicationServiceInterface::class);
+
+        // 検証
+        $this->expectException(AuthenticationException::class);
+
+        // 実行
+        $auth_application_service->login(
+            [
+                'email' => $expected_email,
+                'password' => $expected_password,
+            ]
+        );
+    }
+}
