2201c94ce66465cebbec2434c3864c7a7434778a
Sakyo24 <sakyo.work24@gmail.com>
2023-01-25 21:59:50 +0900

    広告ユニットIDの取得　修正

--- flutter_src/lib/model/admob.dart
+++ flutter_src/lib/model/admob.dart
@@ -1,35 +1,18 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AdMob {
-  static bool isTest = (dotenv.get('AD_TEST_FLAG') == 'true') ? true : false;
-
-  static Map<String, Map<String, String>> productionAdIds = {
+  static Map<String, Map<String, String>> adIds = {
     'ios': {
-      'banner': 'ca-app-pub-9050779314039155/5275884586',
-      'interstitial': 'ca-app-pub-9050779314039155/7123356710',
+      'banner': dotenv.get('IOS_BANNER_ID'),
+      'interstitial': dotenv.get('IOS_INTERSTITIAL_ID')
     },
     'android': {
-      'banner': 'ca-app-pub-9050779314039155/6644806577',
-      'interstitial': 'ca-app-pub-9050779314039155/6588966252',
-    }
-  };
-
-  static Map<String, Map<String, String>> testAdIds = {
-    'ios': {
-      'banner': 'ca-app-pub-3940256099942544/2934735716',
-      'interstitial': 'ca-app-pub-3940256099942544/5135589807'
-    },
-    'android': {
-      'banner': 'ca-app-pub-3940256099942544/6300978111',
-      'interstitial': 'ca-app-pub-3940256099942544/8691691433'
+      'banner': dotenv.get('ANDROID_BANNER_ID'),
+      'interstitial': dotenv.get('ANDROID_INTERSTITIAL_ID')
     }
   };
 
   static String getAdId({required String deviceType, required String adType}) {
-    if (isTest == true) {
-      return testAdIds[deviceType]![adType].toString();
-    } else {
-      return productionAdIds[deviceType]![adType].toString();
-    }
+    return adIds[deviceType]![adType].toString();
   }
 }
