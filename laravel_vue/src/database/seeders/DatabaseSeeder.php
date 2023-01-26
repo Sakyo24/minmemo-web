2b26e762fa8783a82fd6fc714c2b8ddc5e029324
Migisan <ukyo.business.desktop@gmail.com>
2023-01-26 23:27:06 +0900

    fix:戻り値の型定義

--- laravel_vue/src/database/seeders/DatabaseSeeder.php
+++ laravel_vue/src/database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
         if (config('app.env') === 'production') {
             // 本番環境
