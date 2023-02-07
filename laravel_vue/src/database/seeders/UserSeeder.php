9a6b5884d9230d85d73ebf8667efb9b72ab38c81
Sakyo24 <sakyo.work24@gmail.com>
2023-02-07 22:10:29 +0900

    UserSeeder　修正

--- laravel_vue/src/database/seeders/UserSeeder.php
+++ laravel_vue/src/database/seeders/UserSeeder.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Carbon\Carbon;
 
 class UserSeeder extends Seeder
@@ -19,16 +19,19 @@ class UserSeeder extends Seeder
     public function run()
     {
         $datas = [];
+
         for ($i = 1; $i <= 10; $i++) {
             $dt = new Carbon('2021-12-31');
             $this_day = $dt->addDay($i);
             $datas[] = [
-                'name'       => 'ユーザー' . $i,
-                'email'      => 'user' . $i . '@test.ne.jp',
-                'password'   => bcrypt('user' . $i),
-                'deleted_at' => null,
+                'name' => 'ユーザー' . $i,
+                'email' => 'user' . $i . '@test.ne.jp',
+                'email_verified_at' => null,
+                'password' => Hash::make('user' . $i),
+                'remember_token' => null,
                 'created_at' => $this_day,
                 'updated_at' => $this_day,
+                'deleted_at' => null,
             ];
         }
 
