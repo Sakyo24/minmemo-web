171b60dc505f6f518dc46d86dfa553dda2f2ace2
Migisan <ukyo.business.desktop@gmail.com>
2023-03-19 00:39:15 +0900

    fix:ボトムメニューの共通化

--- flutter_src/lib/pages/user/show.dart
+++ flutter_src/lib/pages/user/show.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'dart:convert';
-import 'dart:io';
-import '../../model/admob.dart';
+
+import '../../components/menu.dart';
+import '../../config/constants.dart';
 import '../../utils/network.dart';
-import '../groups/index.dart';
 import '../top.dart';
-import '../index.dart';
 
 class UserShowPage extends StatefulWidget {
   const UserShowPage({super.key});
@@ -18,7 +16,6 @@ class UserShowPage extends StatefulWidget {
 }
 
 class _UserShowPageState extends State<UserShowPage> {
-  final int _currentPage = 2;
   String? _name;
   String? _email;
   bool _isLoading = false;
@@ -30,7 +27,7 @@ class _UserShowPageState extends State<UserShowPage> {
 
     if (user != null) {
       setState(() {
-        _name  = user['name'];
+        _name = user['name'];
         _email = user['email'];
       });
     }
@@ -54,7 +51,9 @@ class _UserShowPageState extends State<UserShowPage> {
     if (res == null) {
       if (mounted) {
         ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text("エラーが発生しました。"))
+          const SnackBar(
+            content: Text("エラーが発生しました。"),
+          ),
         );
       }
       setState(() {
@@ -68,7 +67,13 @@ class _UserShowPageState extends State<UserShowPage> {
       var body = json.decode(res.body);
       if (mounted) {
         ScaffoldMessenger.of(context).showSnackBar(
-          (res.statusCode >= 500 && res.statusCode < 600) ? const SnackBar(content: Text("サーバーエラーが発生しました。")) : SnackBar(content: Text(body['message']))
+          (res.statusCode >= 500 && res.statusCode < 600)
+              ? const SnackBar(
+                  content: Text("サーバーエラーが発生しました。"),
+                )
+              : SnackBar(
+                  content: Text(body['message']),
+                ),
         );
       }
       setState(() {
@@ -84,35 +89,13 @@ class _UserShowPageState extends State<UserShowPage> {
 
     if (!mounted) return;
     Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => const TopPage())
-    );
-  }
-
-  // TODO:広告の共通化
-  // バナー広告
-  late BannerAd bannerAd;
-  bool isAdLoaded = false;
-  void initAd() {
-    this.bannerAd = BannerAd(
-      adUnitId: Platform.isAndroid ? AdMob.getAdId(deviceType: 'android', adType: 'banner') : AdMob.getAdId(deviceType: 'ios', adType: 'banner'),
-      size: AdSize.banner,
-      request: AdRequest(),
-      listener: BannerAdListener(
-        onAdLoaded: (Ad ad) {
-          setState(() {
-            isAdLoaded = true;
-          });
-        }
-      )
-    )..load();
+        context, MaterialPageRoute(builder: (context) => const TopPage()));
   }
 
   @override
   void initState() {
     super.initState();
     _loadUserData();
-    initAd();
   }
 
   @override
@@ -120,66 +103,23 @@ class _UserShowPageState extends State<UserShowPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("マイページ"),
-        automaticallyImplyLeading: false
+        automaticallyImplyLeading: false,
       ),
       body: _isLoading
-      ? const Center(
-        child: CircularProgressIndicator()
-      )
-      : Center(
-        child: Column(
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                _logout();
-              },
-              child: const Text("ログアウト")
-            )
-          ]
-        ),
-      ),
-      // TODO:ボトムメニュー共通化
-      bottomNavigationBar: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Container(
-            width: isAdLoaded ? bannerAd.size.width.toDouble() : 0,
-            height: isAdLoaded ? bannerAd.size.height.toDouble() : 0,
-            child: isAdLoaded ? AdWidget(ad: bannerAd) : Container()
-          ),
-          BottomNavigationBar(
-            currentIndex: _currentPage,
-            items: const [
-              BottomNavigationBarItem(
-                label: 'メモ',
-                icon: Icon(Icons.list)
+          ? const Center(child: CircularProgressIndicator())
+          : Center(
+              child: Column(
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      _logout();
+                    },
+                    child: const Text("ログアウト"),
+                  )
+                ],
               ),
-              BottomNavigationBarItem(
-                label: 'グループ',
-                icon: Icon(Icons.groups)
-              ),
-              BottomNavigationBarItem(
-                label: 'マイページ',
-                icon: Icon(Icons.perm_identity)
-              )
-            ],
-            onTap: (int value) {
-              if (value == 0) {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const IndexPage()),
-                );
-              } else if (value == 1) {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const GroupsIndexPage()),
-                );
-              }
-            }
-          )
-        ]
-      )
+            ),
+      bottomNavigationBar: const Menu(currentPageIndex: PageIndex.user),
     );
   }
 }
