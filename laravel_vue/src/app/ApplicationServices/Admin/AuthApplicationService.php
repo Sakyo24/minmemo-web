8dab6574200c32a39fa1ce8b13b2810dab0808ec
Migisan <ukyo.business.desktop@gmail.com>
2023-04-02 16:24:01 +0900

    add:AdminAuthApplicationServiceにinvite/verifyメソッドの追加

--- laravel_vue/src/app/ApplicationServices/Admin/AuthApplicationService.php
+++ laravel_vue/src/app/ApplicationServices/Admin/AuthApplicationService.php
@@ -4,10 +4,19 @@ declare(strict_types=1);
 
 namespace App\ApplicationServices\Admin;
 
+use App\Mail\Admin\VerificationMail;
+use App\Mail\Admin\VerifiedMail;
 use App\Models\Admin;
 use App\Repositories\AdminRepositoryInterface;
+use Carbon\Carbon;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\URL;
+use RuntimeException;
 
 class AuthApplicationService implements AuthApplicationServiceInterface
 {
@@ -71,5 +80,80 @@ class AuthApplicationService implements AuthApplicationServiceInterface
         }
 
         return null;
+    }
+
+    /**
+     * 管理者の招待
+     *
+     * @param array $input
+     * @return Admin
+     */
+    public function invite(array $input): Admin
+    {
+        // 初期パスワード生成
+        $password = substr(bin2hex(random_bytes(16)), 0, 16);
+
+        $input['password'] = Hash::make($password);
+
+        $admin = DB::transaction(function () use ($input) {
+            $admin = $this->admin_repository->create($input);
+
+            return $admin;
+        });
+
+        // 認証URLの生成
+        $url = URL::temporarySignedRoute(
+            'admin.verification.verify',
+            Carbon::now()->addMinutes(Config::get('auth.verification.expire', 60)),
+            [
+                'id' => $admin->id,
+                'hash' => sha1($admin->email),
+            ]
+        );
+
+        // メール送信
+        Mail::to($admin->email)->send(new VerificationMail($url, $password));
+
+        return $admin;
+    }
+
+    /**
+     * 管理者のメールアドレス認証
+     *
+     * @param array $input
+     * @return void
+     */
+    public function verify(array $input): void
+    {
+        // 管理者の取得
+        $admin = $this->admin_repository->findById($input['id']);
+
+        // IDチェック
+        if (is_null($admin)) {
+            // TODO: 独自例外にしたい
+            throw new RuntimeException('存在しないIDです。');
+        }
+
+        // ハッシュ値チェック
+        if (!hash_equals((string)$input['hash'], sha1($admin->email))) {
+            // TODO: 独自例外にしたい
+            throw new RuntimeException('不正な値です。');
+        }
+
+        // 既に認証済みかチェック
+        if (!is_null($admin->email_verified_at)) {
+            // TODO: 独自例外にしたい
+            throw new RuntimeException('既に認証済みです。');
+        }
+
+        // 認証処理
+        DB::transaction(function () use ($admin) {
+            $admin->forceFill([
+                'email_verified_at' => Carbon::now(),
+            ])->save();
+        });
+
+        // 登録完了メールの送信
+        Mail::to($admin->email)->send(new VerifiedMail());
     }
 }
--- laravel_vue/src/tests/Unit/ApplicationServices/Admin/AuthApplicationServiceTest.php
+++ laravel_vue/src/tests/Unit/ApplicationServices/Admin/AuthApplicationServiceTest.php
@@ -5,12 +5,18 @@ declare(strict_types=1);
 namespace Tests\Unit\ApplicationServices\Admin;
 
 use App\ApplicationServices\Admin\AuthApplicationServiceInterface;
+use App\Mail\Admin\VerificationMail;
+use App\Mail\Admin\VerifiedMail;
 use App\Models\Admin;
 use App\Repositories\AdminRepositoryInterface;
+use Carbon\Carbon;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
 use Mockery;
+use RuntimeException;
 use Tests\TestCase;
 
 class AuthApplicationServiceTest extends TestCase
@@ -178,5 +184,235 @@ class AuthApplicationServiceTest extends TestCase
 
         // 検証
         $this->assertNull($admin);
+    }
+
+    /**
+     * @return void
+     */
+    public function testInvite(): void
+    {
+        // データ
+        $expected_id = 1;
+        $expected_name = Str::random();
+        $expected_email = Str::random() . '@example.com';
+        $expected_password = Str::random();
+        $expected_url = Str::random();
+
+        $expected_admin = Admin::factory()->make([
+            'id' => $expected_id,
+            'name' => $expected_name,
+            'email' => $expected_email,
+            'password' => $expected_password,
+        ]);
+
+        // モックの設定
+        $this->admin_repository_mock->shouldReceive('create')
+            ->once()
+            ->andReturn($expected_admin);
+
+        $this->app->instance(AdminRepositoryInterface::class, $this->admin_repository_mock);
+
+        // インスタンス生成
+        $auth_application_service = $this->app->make(AuthApplicationServiceInterface::class);
+
+        // 実行
+        $admin = $auth_application_service->invite([
+            'name' => $expected_name,
+            'email' => $expected_email,
+        ]);
+
+        // 検証
+        $this->assertSame($expected_id, $admin->id);
+        $this->assertSame($expected_name, $admin->name);
+        $this->assertSame($expected_email, $admin->email);
+
+        // TODO: 本当はモック化したいが、モック化すると内容の検証ができない。
+        $mailable = new VerificationMail($expected_url, $expected_password);
+        // TODO: 上手く動作しない
+        // $mailable->assertTo($expected_email);
+        $mailable->assertHasSubject('管理画面への招待');
+        $mailable->assertSeeInHtml($expected_url);
+        $mailable->assertSeeInHtml($expected_password);
+        $mailable->assertSeeInHtml('管理者として招待されました。');
+    }
+
+    /**
+     * @return void
+     */
+    public function testVerify(): void
+    {
+        // データ
+        $expected_id = 2;
+        $expected_email = Str::random() . '@example.com';
+        $expected_name = Str::random();
+        $expected_hash = sha1($expected_email);
+        $expected_password = Str::random();
+
+        $expected_admin = Admin::factory()->make([
+            'id' => $expected_id,
+            'name' => $expected_name,
+            'email' => $expected_email,
+            'password' => $expected_password,
+            'email_verified_at' => null,
+        ]);
+
+        // モックの設定
+        $this->admin_repository_mock->shouldReceive('findById')
+            ->once()
+            ->with($expected_id)
+            ->andReturn($expected_admin);
+
+        $this->app->instance(AdminRepositoryInterface::class, $this->admin_repository_mock);
+
+        // インスタンス生成
+        $auth_application_service = $this->app->make(AuthApplicationServiceInterface::class);
+
+        // 実行
+        $auth_application_service->verify([
+            'id' => $expected_id,
+            'hash' => $expected_hash,
+        ]);
+
+        // 検証
+        // TODO: 本当はモック化したいが、モック化すると内容の検証ができない。
+        $mailable = new VerifiedMail();
+        // TODO: 上手く動作しない
+        // $mailable->assertTo($expected_email);
+        $mailable->assertHasSubject('メールアドレスの認証完了');
+        $mailable->assertSeeInHtml('管理者としてのメールアドレスの認証が完了しました。');
+    }
+
+    /**
+     * @return void
+     */
+    public function testVerifyIdError(): void
+    {
+        // モック
+        Mail::fake();
+
+        // データ
+        $expected_id = 1;
+        $expected_email = Str::random() . '@example.com';
+        $expected_hash = sha1($expected_email);
+
+        // モックの設定
+        $this->admin_repository_mock->shouldReceive('findById')
+            ->once()
+            ->with($expected_id)
+            ->andReturn(null);
+
+        $this->app->instance(AdminRepositoryInterface::class, $this->admin_repository_mock);
+
+        // インスタンス生成
+        $auth_application_service = $this->app->make(AuthApplicationServiceInterface::class);
+
+        // 検証
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('存在しないIDです。');
+
+        // 実行
+        $auth_application_service->verify([
+            'id' => $expected_id,
+            'hash' => $expected_hash,
+        ]);
+
+        // 検証
+        Mail::assertNothingSent();
+    }
+
+    /**
+     * @return void
+     */
+    public function testVerifyHashError(): void
+    {
+        // モック
+        Mail::fake();
+
+        // データ
+        $expected_id = 1;
+        $expected_name = Str::random();
+        $expected_email = Str::random() . '@example.com';
+        $expected_password = Str::random();
+        $expected_hash = sha1(Str::random());
+
+        $expected_admin = Admin::factory()->make([
+            'id' => $expected_id,
+            'name' => $expected_name,
+            'email' => $expected_email,
+            'password' => $expected_password,
+            'email_verified_at' => null,
+        ]);
+
+        // モックの設定
+        $this->admin_repository_mock->shouldReceive('findById')
+            ->once()
+            ->with($expected_id)
+            ->andReturn($expected_admin);
+
+        $this->app->instance(AdminRepositoryInterface::class, $this->admin_repository_mock);
+
+        // インスタンス生成
+        $auth_application_service = $this->app->make(AuthApplicationServiceInterface::class);
+
+        // 検証
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('不正な値です。');
+
+        // 実行
+        $auth_application_service->verify([
+            'id' => $expected_id,
+            'hash' => $expected_hash,
+        ]);
+
+        // 検証
+        Mail::assertNothingSent();
+    }
+
+    /**
+     * @return void
+     */
+    public function testVerifyEmailVerifiedAtError(): void
+    {
+        // モック
+        Mail::fake();
+
+        // データ
+        $expected_id = 1;
+        $expected_name = Str::random();
+        $expected_email = Str::random() . '@example.com';
+        $expected_password = Str::random();
+        $expected_hash = sha1($expected_email);
+
+        $expected_admin = Admin::factory()->make([
+            'id' => $expected_id,
+            'name' => $expected_name,
+            'email' => $expected_email,
+            'password' => $expected_password,
+            'email_verified_at' => Carbon::now(),
+        ]);
+
+        // モックの設定
+        $this->admin_repository_mock->shouldReceive('findById')
+            ->once()
+            ->with($expected_id)
+            ->andReturn($expected_admin);
+
+        $this->app->instance(AdminRepositoryInterface::class, $this->admin_repository_mock);
+
+        // インスタンス生成
+        $auth_application_service = $this->app->make(AuthApplicationServiceInterface::class);
+
+        // 検証
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('既に認証済みです。');
+
+        // 実行
+        $auth_application_service->verify([
+            'id' => $expected_id,
+            'hash' => $expected_hash,
+        ]);
+
+        // 検証
+        Mail::assertNothingSent();
     }
 }
