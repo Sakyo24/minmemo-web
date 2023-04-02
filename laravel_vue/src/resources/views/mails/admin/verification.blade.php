c501cd267c8c0870d6f2c77b020a9958e742ed0c
Migisan <ukyo.business.desktop@gmail.com>
2023-04-02 21:15:40 +0900

    fix:メール修正

--- laravel_vue/src/resources/views/mails/admin/verification.blade.php
+++ laravel_vue/src/resources/views/mails/admin/verification.blade.php
@@ -1,4 +1,8 @@
-管理者として招待されました。
+<div>
+  <p>管理者として招待されました。</p>
 
-認証URL：{{ $url }}
-初期パスワード：{{ $password }}
+  <ul>
+    <li>認証URL：<a href="{{ $url }}" target="_blank" rel="noopener noreferrer">{{ $url }}</a></li>
+    <li>初期パスワード：{{ $password }}</li>
+  </ul>
+</div>
